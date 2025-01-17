782b9d6348dafac7478325afc195c43dc68dae08
ShreyX <134378619+ShreyX24@users.noreply.github.com>
2025-01-17 11:06:52 +0530

    updated main.py

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from routes import user
 
+
+
 app = FastAPI(
     
     title="User Management API",
     description="API for managing user profiles with MongoDB backend",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:5173",     # Development frontend
+    # "https://yourapp.vercel.app" # Production frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers
